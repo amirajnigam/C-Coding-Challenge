13dc8dc3c09fe6e5338197559c78aca35b1cb7b2
Amiraj Nigam <amirajnigam@gmail.com>
2020-07-01 00:28:50 -0700

    size of pointer int char

---
+++ Day 28/day28_ptrsize.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int iVal;
+    char cVal;
+    void *ptr;
+    iVal = 50; cVal= 65;
+    
+    ptr = &iVal;
+    printf("value = %d, size = %d\n",*(int*)ptr,sizeof(ptr));
+    
+    ptr=&cVal;
+    printf("value = %d,size=%d\n",*(char*)ptr,sizeof(ptr));
+    
+    return 0;
+}
