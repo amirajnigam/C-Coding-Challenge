521fae5f1cb1979c61dc03ee03f7bdcab5774fba
Amiraj Nigam <amirajnigam@gmail.com>
2020-08-10 22:25:07 -0700

    structure padding and infinite while loop

---
+++ Day 19/day19_structurepadding.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+struct CDAC{
+    char a,b;
+    int i;
+    char x;
+    char y;
+}ShivamCDAC;
+
+int main()
+{
+    printf("Size is %lu", sizeof(ShivamCDAC));
+    return 0;
+}
---
+++ Day 20/day20_infiniteloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0.2;
+    while(i!=1)
+    {
+        printf("#ShivamCDAC");
+        i+=0.2;
+    }
+    return 0;
+}
